d9e1f4b7803b63b2754d12487b2d99db6a266c59
AdamBergkvist1 <adambergkvist16@gmail.com>
2026-03-08 20:07:09 +0100

    AI Testcases

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,15 @@ module.exports = {
         '^.+\\.ts?$': 'ts-jest'
         },
     //collectCoverage: true,
+
+};
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    "Game_mechanics/**/*.{js,ts}",
+    "API/**/*.{js,ts}",
+    "!lib/**" // Utropstecknet betyder "exkludera detta"
+  ],
 };
